bf3f66f5bac122c712b103803e761f0a711f26e2
iamahmadpk <iamahmadpk53@gmail.com>
2023-02-17 17:52:51 +0500

    Using List

--- LINQ Basics/Program.cs
+++ LINQ Basics/Program.cs
@@ -1,10 +1,15 @@
-﻿using System;
-
+﻿var result = GenerateNumbers(10).Where(n=>n%3==0);
+foreach (var number in result)
+{
+    Console.WriteLine(number);
+}
+Console.WriteLine(result);
 IEnumerable<int> GenerateNumbers(int max)
 {
     var numbers = new List<int>();
-    if ()
+    for(int i = 0; i<= max; i++)
     {
-
+        numbers.Add(i);
     }
+    return numbers;
 }
